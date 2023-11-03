201ae5f7bf140b3fac9dd5e428bc2cb42ec07ea6
Sidoryak-Ostap <ostapsidorak@gmail.com>
2023-11-03 12:16:59 +0200

    test branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+/*test branch*/
 function App() {
   return (
     <div className="App">
