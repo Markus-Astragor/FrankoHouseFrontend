04fb55f4bb261416c579db01bce94d7e54cc9826
Sidoryak-Ostap <ostapsidorak@gmail.com>
2023-11-03 12:19:32 +0200

    markiian test branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+/*add markiian branch */
 function App() {
   return (
     <div className="App">
