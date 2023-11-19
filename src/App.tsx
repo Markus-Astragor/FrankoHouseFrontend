b727364684f7df16b76124083f9fe6561bd7a41d
Ostap-Sidoryak <ostapsidorak@gmail.com>
2023-11-19 23:16:24 +0200

    [eslint & prettier configuration]

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Route, Routes, BrowserRouter as Router, NavLink } from 'react-router-dom';
-import './App.css';
-import MainPage from './Pages/MainPage/MainPage';
-import NewsBoard from './Pages/NewsBoard/NewsBoard';
-
+import React from "react";
+import styled from "styled-components";
+import { Route, Routes, BrowserRouter as Router, NavLink } from "react-router-dom";
+import "./App.css";
+import MainPage from "./Pages/MainPage/MainPage";
+import NewsBoard from "./Pages/NewsBoard/NewsBoard";
 
 function App() {
   const obj = {
-    color: 'black',
-    fontSize: '30px',
-    textDecoration: 'none' 
-  }
+    color: "black",
+    fontSize: "30px",
+    textDecoration: "none",
+  };
 
   const Button = styled.button`
     color: red;
-  `
+  `;
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
-      <div>
-          <NavLink to='/' style={obj}><Button>main</Button><br/></NavLink>
-          <NavLink to='/posts' style={obj}>posts<br/></NavLink>       
+        <div>
+          <NavLink to='/' style={obj}>
+            <Button>main</Button>
+            <br />
+          </NavLink>
+          <NavLink to='/posts' style={obj}>
+            posts
+            <br />
+          </NavLink>
         </div>
         <Routes>
-          <Route element={<MainPage/>} path='/'/>
-          <Route element={<NewsBoard/>} path='/posts'/>
-
+          <Route element={<MainPage />} path='/' />
+          <Route element={<NewsBoard />} path='/posts' />
         </Routes>
       </Router>
     </div>
