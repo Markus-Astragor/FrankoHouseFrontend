4c6f22e6d71bcff4795565187fe46bd0d08c2a9b
Marko Kosyk <kosyk.markian@gmail.com>
2023-12-07 12:53:25 +0200

    [Changes] need to be corrected

--- src/Pages/NewsBlock/NewsBoard/mocks.ts
+++ src/Pages/NewsBlock/NewsBoard/mocks.ts
@@ -8,14 +8,17 @@ interface NewsMockData {
 }
 
 export const data: NewsMockData[] = [];
+
 const mockDataLength = 20;
+
 for (let i = 1; i < mockDataLength; i++) {
   const mockObj: NewsMockData = {
     image: TeenagerImg,
     title: `«Тінейджер в музеї ${i}»`,
+    // description
     shortText:
       "В Домі Франка тривала Літня школа музейних лідерів ««Тінейджер в музеї»» для учнів львівських шкіл віком від 10 до 14 років. Мета літньої школи – підвищити інтерес підлітків до культурної спадщини, сприяти їхній соціальній інтеграції та комунікації між особами з різними зацікавленнями та фізичними можливостями....",
-    timeStamp: "10–15 червня 2019 р",
+    timeStamp: "10–15 червня 2019 р", // date
   };
 
   data.push(mockObj);
