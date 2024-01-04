5e4f159a725498d286618e72e1f20172bef05490
Marko Kosyk <kosyk.markian@gmail.com>
2024-01-04 19:31:19 +0200

    [Changes] loader, errorHandling added

--- src/Pages/AdminAuth/AdminAuth.tsx
+++ src/Pages/AdminAuth/AdminAuth.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { MainBlock, InputElement, Form, FieldName, SubmitButton } from "./AdminAuthStyle";
+import {
+  MainBlock,
+  InputElement,
+  Form,
+  FieldName,
+  SubmitButton,
+  ErrorParagraph,
+  Loader,
+  BlockForLoader,
+} from "./AdminAuthStyle";
 import axios from "axios";
 import URLS from "../../configURLS.json";
 
@@ -7,6 +16,7 @@ function AdminAuth() {
   const [login, setLogin] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [loader, setLoader] = useState<boolean>(false);
+  const [errorText, setErrorText] = useState<string>("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,22 +26,50 @@ function AdminAuth() {
     console.log("password", password);
 
     axios
-      .post(`${URLS["BASE-URL"]}`, {})
+      .post(`${URLS["BASE-URL"]}/login`, {
+        Login: login,
+        Password: password,
+      })
       .then((res) => {
         console.log("res", res);
+        localStorage.setItem("jwtToken", res.data.token);
       })
-      .catch((err) => console.log("err", err))
-      .finally(() => setLoader(false));
+      .catch((err) => {
+        console.log("err", err);
+        setErrorText(err.response.data.message);
+      })
+      .finally(() => {
+        setLoader(false);
+        setLogin("");
+        setPassword("");
+      });
   };
 
   return (
     <MainBlock>
       <Form onSubmit={handleSubmit}>
         <FieldName>Логін</FieldName>
-        <InputElement type='text' onChange={(e) => setLogin(e.target.value)} />
+        <InputElement
+          type='text'
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          required
+        />
         <FieldName>Пароль</FieldName>
-        <InputElement type='password' onChange={(e) => setPassword(e.target.value)} />
-        {loader ? <span>Loading...</span> : <SubmitButton>Увійти</SubmitButton>}
+        <InputElement
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <ErrorParagraph>{errorText}</ErrorParagraph>
+        {loader ? (
+          <BlockForLoader>
+            <Loader></Loader>
+          </BlockForLoader>
+        ) : (
+          <SubmitButton>Увійти</SubmitButton>
+        )}
       </Form>
     </MainBlock>
   );
