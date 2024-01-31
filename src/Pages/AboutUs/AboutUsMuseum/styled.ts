f2f7085f026c190b9a274bbe75843a0c15926ce4
Marko Kosyk <kosyk.markian@gmail.com>
2024-01-31 14:18:14 +0200

    [Changes] About us page added + changing icon

---
+++ src/Pages/AboutUs/AboutUsMuseum/styled.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const AboutUsMuseumBlock = styled.div`
+  @import url("https://fonts.cdnfonts.com/css/kelly-slab");
+  width: 80%;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+export const AboutUsMuseumContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const AboutUsLeftPart = styled.div`
+  width: 55%;
+`;
+
+export const AboutUsTitle = styled.h2`
+  margin-left: 20px;
+  margin-top: 123px;
+  font-size: 53px;
+  font-family: "Kelly Slab", sans-serif;
+  font-weight: 100;
+`;
+
+export const TextContent = styled.p`
+  margin-top: 41px;
+  font-family: "Montserrat";
+  font-size: 20px;
+  color: black;
+  font-weight: 600;
+`;
+
+export const AboutUsRightPart = styled.div`
+  width: 45%;
+  position: relative;
+`;
+
+export const FrankoUSSRimg = styled.img`
+  display: block;
+  margin: 207px 0px 0px 300px;
+`;
+
+export const ChurchImg = styled.div`
+  position: absolute;
+  bottom: 149px;
+  left: 144px;
+`;
--- src/Pages/AboutUs/AboutUsStyle.tsx
+++ src/Pages/AboutUs/AboutUsStyle.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const AboutUsStyled = styled.div`
-  background-color: #f3eded;
+  background-color: white;
 `;
