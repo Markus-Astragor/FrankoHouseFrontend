7264c413a7788b38ff02915891a5a3b88aed299c
Marko Kosyk <kosyk.markian@gmail.com>
2024-02-11 00:10:45 +0200

    [Changes] our projects updated + responsive

--- src/Pages/NewsBlock/NewsBoard/NewsBoard.tsx
+++ src/Pages/NewsBlock/NewsBoard/NewsBoard.tsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { BlockForLoader, NewsBlock, NewsContainer, TitleProjects } from "./NewsBoardStyle";
 import NewsPiece from "../NewsPiece/NewsPiece";
-import Pagination from "./Pagination/Pagination";
+
 import URLS from "../../../configURLS.json";
 import LoaderComponent from "../../../components/Loader/LoaderComponent";
 
@@ -10,14 +15,9 @@ function NewsBoard() {
   type NewsData = {
     _id: string;
     title: string;
-    shortDescription: string;
     photos: string;
-    timestamp: string;
   };
 
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [newsPerPage] = useState<number>(6);
-  const [page, setPage] = useState<number>(1);
   const [dataFromBackend, setDataFromBackend] = useState<NewsData[]>([]);
   const [loader, setLoader] = useState<boolean>(false);
 
@@ -37,21 +37,66 @@ function NewsBoard() {
       });
   }, []);
 
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value);
-    setCurrentPage(value);
+  const settings = {
+    dots: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <></>,
+    prevArrow: <></>,
+    responsive: [
+      {
+        breakpoint: 1780,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerPadding: "150px",
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerPadding: "150px",
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "160px",
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "140px",
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "40px",
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "20px",
+        },
+      },
+    ],
   };
+  console.log("dataFromBackend", dataFromBackend);
 
-  const indexOfLastNewsPiece: number = currentPage * newsPerPage;
-  const indexOfFirstNewsPiece = indexOfLastNewsPiece - newsPerPage;
-  const currentNewsForCurrentPage = dataFromBackend.slice(
-    indexOfFirstNewsPiece,
-    indexOfLastNewsPiece,
-  );
-  const totalCount =
-    dataFromBackend.length % newsPerPage !== 0
-      ? Math.floor(dataFromBackend.length / newsPerPage) + 1
-      : dataFromBackend.length / newsPerPage;
   return (
     <>
       <NewsBlock>
@@ -63,21 +108,17 @@ function NewsBoard() {
         ) : (
           <>
             <NewsContainer>
-              {currentNewsForCurrentPage.map((piecenews, index) => {
-                return (
+              <Slider {...settings}>
+                {dataFromBackend?.map((newsPiece, index) => (
                   <NewsPiece
-                    image={piecenews.photos}
-                    title={piecenews.title}
-                    description={piecenews.shortDescription}
-                    date={piecenews.timestamp}
+                    image={newsPiece.photos}
+                    title={newsPiece.title}
+                    id={newsPiece._id}
                     key={index}
-                    id={piecenews._id}
                   />
-                );
-              })}
+                ))}
+              </Slider>
             </NewsContainer>
-
-            <Pagination totalCount={totalCount} page={page} handleChange={handleChange} />
           </>
         )}
       </NewsBlock>
--- src/Pages/NewsBlock/NewsPiece/NewsPiece.tsx
+++ src/Pages/NewsBlock/NewsPiece/NewsPiece.tsx
@@ -1,16 +1,5 @@
 import React from "react";
-import {
-  NewsPiece,
-  ImageForNewsPiece,
-  MiddleImageForNewsPiece,
-  Title,
-  ShortText,
-  TimeStamp,
-  MiddleBlock,
-  WrapperForShortDescription,
-} from "../NewsPiece/styled";
-import MiddleImage from "../../../assets/NewsBoardImages/Середина.png";
-import { useNavigate } from "react-router-dom";
+import { NewsPiece, BlockForImage, BlockForTitle } from "../NewsPiece/styled";
 import moment from "moment";
 import "moment/locale/uk";
 
@@ -18,34 +7,17 @@ type NewsPieceBlockProps = {
   id: string;
   image: string;
   title: string;
-  description: string;
-  date: string;
 };
 
-function NewsPieceBlock({ image, title, description, date, id }: NewsPieceBlockProps) {
-  moment.locale("uk");
-  const formattedDate = moment(date).format("HH:mm | DD MMMM YYYY");
+function NewsPieceBlock({ image, title, id }: NewsPieceBlockProps) {
+  console.log(id);
 
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate(`/posts/${id}`);
-  };
+  moment.locale("uk");
 
   return (
     <NewsPiece>
-      <MiddleBlock>
-        <ImageForNewsPiece>
-          <img src={image} width='400px' />
-        </ImageForNewsPiece>
-        <MiddleImageForNewsPiece>
-          <img src={MiddleImage} />
-          <Title onClick={handleClick}>{title}</Title>
-        </MiddleImageForNewsPiece>
-      </MiddleBlock>
-      <ShortText>
-        <WrapperForShortDescription>{description}</WrapperForShortDescription>
-        <TimeStamp>{formattedDate}</TimeStamp>
-      </ShortText>
+      <BlockForImage src={image} />
+      <BlockForTitle>{title}</BlockForTitle>
     </NewsPiece>
   );
 }
