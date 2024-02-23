0adf945273faf778b6a6f4679a41ed64950d3fa4
Marko Kosyk <kosyk.markian@gmail.com>
2024-02-23 21:24:26 +0200

    [Changes] new translation of page about us

--- src/Pages/AboutUs/AboutUsMuseum/AboutUsMuseum.tsx
+++ src/Pages/AboutUs/AboutUsMuseum/AboutUsMuseum.tsx
@@ -11,30 +11,16 @@ import {
 } from "./styled";
 import FrankoUSSR from "../../../assets/about-us/about-us-info/franko-ussr.png";
 import Church from "../../../assets/about-us/about-us-info/church.png";
+import { useTranslation } from "react-i18next";
 
 export default function AboutUsMuseum() {
+  const { t } = useTranslation();
   return (
     <AboutUsMuseumBlock id='about-us'>
       <AboutUsMuseumContainer>
         <AboutUsLeftPart>
-          <AboutUsTitle>Про нас</AboutUsTitle>
-          <TextContent>
-            Асоціація Музеїв Івана Франка» - це неприбуткова громадська організація, яка працює над
-            налагодженням співпраці із музеями Івана Франка для спільної роботи над увічненням
-            пам’яті видатного українського письменника, мислителя, громадсько-політичного діяча
-            Івана Франка, дослідження та популяризація його духовної спадщини в Україні та поза її
-            межами.
-            <br />
-            <br />
-            Безпосередні джерела фінансування «Асоціації Музеїв Івана Франка» відсутні. Громадська
-            організація здійснює свою діяльність у рамках отриманих грантових проєктів та
-            благодійних внесків.
-            <br />
-            <br />
-            Матеріально-технічна база та кадрове забезпечення відсутні. Діяльність організації
-            здійснюється на базі «Львівського національного літературно-меморіального музею Івана
-            Франка».
-          </TextContent>
+          <AboutUsTitle>{t("ns1.description.about-us.about-us-title")}</AboutUsTitle>
+          <TextContent>{t("ns1.description.about-us.general-info")}</TextContent>
         </AboutUsLeftPart>
         <AboutUsRightPart>
           <FrankoUSSRimg src={FrankoUSSR} />
