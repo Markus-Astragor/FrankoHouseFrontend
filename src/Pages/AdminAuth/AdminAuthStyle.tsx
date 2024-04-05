dc95c31ea89690790c2dd5422e66e5dba6eebc01
Marko Kosyk <kosyk.markian@gmail.com>
2024-04-05 16:40:17 +0300

    [Changes] admin auth panel done

--- src/Pages/AdminAuth/AdminAuthStyle.tsx
+++ src/Pages/AdminAuth/AdminAuthStyle.tsx
@@ -1,60 +1,27 @@
 import styled from "styled-components";
 
-export const MainBlock = styled.div`
+export const CenterBLock = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-export const Form = styled.form`
-  width: 20%;
-  display: flex;
-  justify-content: center;
   flex-direction: column;
-
-  * {
-    margin: 5px;
-  }
-
-  @media (max-width: 550px) {
-    width: 70%;
-  }
-
-  @media (min-width: 551px) and (max-width: 800px) {
-    width: 60%;
-  }
-
-  @media (min-width: 801px) and (max-width: 1200px) {
-    width: 40%;
-  }
-  @media (min-width: 1201px) and (max-width: 1600px) {
-    width: 30%;
-  }
 `;
 
-export const InputElement = styled.input`
-  display: block;
-`;
-
-export const FieldName = styled.span`
-  width: 100%;
-  display: block;
-`;
-
-export const SubmitButton = styled.button`
-  display: block;
-  width: 50%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 5px;
-  margin-bottom: 5px;
-`;
-
-export const ErrorParagraph = styled.p`
-  color: red;
-  font-size: 16px;
-  text-align: center;
-  margin: 5px 0px;
+export const LoginBtn = styled.button`
+  width: fit-content;
+  height: fit-content;
+  padding: 0.83vh 4.44vw;
+  background-color: #09282c;
+  font-size: 1.1rem;
+  border-radius: 0.34vw;
+  color: white;
+  border: 2px solid #09282c;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: #0f363b;
+    border: 2px solid #0f363b;
+  }
 `;
